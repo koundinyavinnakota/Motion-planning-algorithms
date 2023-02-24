c609c152baec1607aa384fc7106115d30f5fdb0a
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2023-02-24 08:44:15 -0500

    Added the map image into the readme

--- Environment/map.py
+++ Environment/map.py
@@ -5,12 +5,11 @@ scale = 3
 c = 5 # Scale of the map
 layout = np.zeros((100*c,100*c,3))
 
-
-
+# Obstacle 1 
 obs1 = np.array([[[25*c,100*c], [25*c,10*c],[30*c,10*c],[30*c,100*c]]])
 cv2.fillPoly(layout,pts = [obs1],color = (255,255,255))
 
-
+#obstacle 2
 obs1 = np.array([[[75*c,0*c], [75*c,90*c],[80*c,90*c],[80*c,0*c]]])
 cv2.fillPoly(layout,pts = [obs1],color = (255,255,255))
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Motion-planning-algorithms
+
+# Dependencies
+-   Python
+-   Opencv 
+-   Numpy
+
+# Environment 
+<img src="Resources/map.jpg" width="500" alt="Alt text" title="Environment">
