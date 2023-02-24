6cc4f6548c5e340a166756d490821eafd74967f8
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2023-02-24 08:32:12 -0500

    Created a simple map with two static obstacles

---
+++ Environment/map.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+scale = 3
+c = 5 # Scale of the map
+layout = np.zeros((100*c,100*c,3))
+
+
+
+obs1 = np.array([[[25*c,100*c], [25*c,10*c],[30*c,10*c],[30*c,100*c]]])
+cv2.fillPoly(layout,pts = [obs1],color = (255,255,255))
+
+
+obs1 = np.array([[[75*c,0*c], [75*c,90*c],[80*c,90*c],[80*c,0*c]]])
+cv2.fillPoly(layout,pts = [obs1],color = (255,255,255))
+
+cv2.imshow(" layout ", layout)
+cv2.waitKey(0)
